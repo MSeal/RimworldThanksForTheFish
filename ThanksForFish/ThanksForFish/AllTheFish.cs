a506685dcec1369600201876ebb53e90c1567163
Matthew Seal <1863892+MSeal@users.noreply.github.com>
2018-02-13 01:25:38 -0800

    Cleaned up xml definitions. Attempted to add job driver (not working atm)

--- ThanksForFish/ThanksForFish/AllTheFish.cs
+++ ThanksForFish/ThanksForFish/AllTheFish.cs
@@ -6,11 +6,6 @@ using Verse.AI;
 using UnityEngine;
 
 namespace AllTheFish {
-    [DefOf]
-    public static class WorkTypeDefOf {
-        public static WorkTypeDef Fishing;
-    }
-
     public class PlaceWorker_DeepWaterFishing : PlaceWorker {
         public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null) {
             TerrainDef terrainDef = map.terrainGrid.TerrainAt(loc);
@@ -21,10 +16,6 @@ namespace AllTheFish {
             return new AcceptanceReport("AllTheFish.DeepWaterFishing".Translate());
         }
     }
-
-    //TODO fishing poles
-
-    //TODO fish instead of meat from catch
 
     //TODO check expanded work tab for subtask
 
@@ -52,6 +43,64 @@ namespace AllTheFish {
 
         public override bool AllowUnreachable {
             get { return true; }
+        }
+    }
+
+    public class JobDriver_CatchFish : JobDriver_DoBill {
+        public Mote fishingRod;
+
+        protected ThingDef GetRodMote(Rot4 rotation) {
+            if (rotation == Rot4.North) {
+                Log.Message("Found North Rod: " + ThingDef.Named("FishingRodNorth"));
+                return ThingDef.Named("FishingRodNorth");
+            }
+            if (rotation == Rot4.East) {
+                Log.Message("Found East Rod: " + ThingDef.Named("FishingRodEast"));
+                return ThingDef.Named("FishingRodEast");
+            }
+            if (rotation == Rot4.South) {
+                Log.Message("Found South Rod: " + ThingDef.Named("FishingRodSouth"));
+                return ThingDef.Named("FishingRodSouth");
+            }
+            if (rotation == Rot4.West) {
+                Log.Message("Found West Rod: " + ThingDef.Named("FishingRodWest"));
+                return ThingDef.Named("FishingRodWest");
+            }
+            return ThingDef.Named("FishingRodNorth");
+        }
+
+        protected override IEnumerable<Toil> MakeNewToils() {
+            var i = 0;
+            Log.Message("START!");
+            foreach (var toil in base.MakeNewToils()) {
+                Log.Message("Toil: " + i + " => " + toil);
+                // Hack Alert: The 13th toil is Toils_Recipe.DoRecipeWork
+                if (i == 12) {
+                    toil.AddPreInitAction(() => {
+                        Thing fishingSpot = this.TargetThingA;
+                        IntVec3 fishingSpotCell = fishingSpot.Position + new IntVec3(0, 0, -1).RotatedBy(fishingSpot.Rotation);
+                        ThingDef mote = GetRodMote(fishingSpot.Rotation);
+                        this.fishingRod = (Mote)ThingMaker.MakeThing(mote, null);
+                        this.fishingRod.exactPosition = fishingSpotCell.ToVector3Shifted();
+                        this.fishingRod.Scale = 1f;
+                        GenSpawn.Spawn(this.fishingRod, fishingSpotCell, this.Map);
+                    });
+                    toil.AddFinishAction(() => {
+                        Thing fishingSpot = this.TargetThingA;
+                        IntVec3 fishingSpotCell = fishingSpot.Position + new IntVec3(0, 0, -1).RotatedBy(fishingSpot.Rotation);
+                        if (this.fishingRod != null) {
+                            this.fishingRod.Destroy();
+                        }
+                        foreach (var thing in base.Map.thingGrid.ThingsListAt(fishingSpotCell)) {
+                            if (thing.def.defName.Contains("FishingRod")) {
+                                thing.DeSpawn();
+                            }
+                        }
+                    });
+                }
+                yield return toil;
+                i++;
+            }
         }
     }
 }
