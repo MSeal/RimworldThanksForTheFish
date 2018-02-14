b125be4c8d5510144fc3227b88ba68e6ec5e4417
Matthew Seal <1863892+MSeal@users.noreply.github.com>
2018-02-13 23:52:26 -0800

    Got all basic tasks completed. Finally worked out how to add fishing rods

--- ThanksForFish/ThanksForFish/AllTheFish.cs
+++ ThanksForFish/ThanksForFish/AllTheFish.cs
@@ -17,32 +17,56 @@ namespace AllTheFish {
         }
     }
 
-    //TODO check expanded work tab for subtask
-
-    public class WorkGiver_CatchFish : WorkGiver_Scanner {
-        public override ThingRequest PotentialWorkThingRequest {
-            get { return ThingRequest.ForDef(ThingDef.Named("FishingSpot")); }
+    public class WorkGiver_CatchFish : WorkGiver_DoBill {
+        // Thanks for making the more advanced version of this private... :/
+        protected bool ThingIsUsableBillGiver(Thing thing) {
+            Pawn pawn = thing as Pawn;
+            if (this.def.fixedBillGiverDefs != null && this.def.fixedBillGiverDefs.Contains(thing.def))
+            {
+                return true;
+            }
+            if (pawn != null)
+            {
+                return (this.def.billGiversAllHumanlikes && pawn.RaceProps.Humanlike) ||
+                    (this.def.billGiversAllMechanoids && pawn.RaceProps.IsMechanoid) ||
+                    (this.def.billGiversAllAnimals && pawn.RaceProps.Animal);
+            }
+            return false;
         }
 
-        public override PathEndMode PathEndMode {
-            get { return PathEndMode.InteractionCell; }
-        }
-
-        public override bool HasJobOnThing(Pawn pawn, Thing fishingSpot, bool forced = false) {
-            if (fishingSpot.def.defName != "FishingSpot") { return false; }
-            if (fishingSpot.IsBurning()) { return false; }
-            if (pawn.Dead || pawn.Downed || pawn.IsBurning()) { return false; }
-            if (pawn.CanReserveAndReach(fishingSpot, this.PathEndMode, Danger.Some) == false) { return false; }
-            return true;
+        protected Job AssignBill(Job job, Pawn pawn, IBillGiver giver) {
+            for (int i = 0; i < giver.BillStack.Count; i++) {
+                Bill bill = giver.BillStack[i];
+                if (bill.recipe.requiredGiverWorkType == null || bill.recipe.requiredGiverWorkType == this.def.workType) {
+                    if (bill.ShouldDoNow() && bill.PawnAllowedToStartAnew(pawn)) {
+                        if (!bill.recipe.PawnSatisfiesSkillRequirements(pawn)) {
+                            JobFailReason.Is("MissingSkill".Translate());
+                        }
+                        job.bill = bill;
+                        return job;
+                    }
+                }
+            }
+            return null;
         }
 
         public override Job JobOnThing(Pawn pawn, Thing fishingSpot, bool forced = false) {
-            IntVec3 fishingSpotCell = fishingSpot.Position + new IntVec3(0, 0, -1).RotatedBy(fishingSpot.Rotation);
-            return new Job(DefDatabase<JobDef>.GetNamed("CatchFish"), fishingSpot, fishingSpotCell);
-        }
-
-        public override bool AllowUnreachable {
-            get { return true; }
+            IBillGiver billGiver = fishingSpot as IBillGiver;
+            if (billGiver != null &&
+                    this.ThingIsUsableBillGiver(fishingSpot) &&
+                    billGiver.BillStack.AnyShouldDoNow &&
+                    billGiver.CurrentlyUsableForBills()) {
+                LocalTargetInfo target = fishingSpot;
+                if (pawn.CanReserve(target, 1, -1, null, forced) && !fishingSpot.IsBurning() && !fishingSpot.IsForbidden(pawn)) {
+                    IntVec3 fishingSpotCell = fishingSpot.Position + new IntVec3(0, 0, -1).RotatedBy(fishingSpot.Rotation);
+                    billGiver.BillStack.RemoveIncompletableBills();
+                    Job job = new Job(DefDatabase<JobDef>.GetNamed("CatchFish"), fishingSpot, fishingSpotCell);
+                    if (this.AssignBill(job, pawn, billGiver) != null) {
+                        return job;
+                    }
+                }
+            }
+            return null;
         }
     }
 
@@ -51,43 +75,37 @@ namespace AllTheFish {
 
         protected ThingDef GetRodMote(Rot4 rotation) {
             if (rotation == Rot4.North) {
-                Log.Message("Found North Rod: " + ThingDef.Named("FishingRodNorth"));
                 return ThingDef.Named("FishingRodNorth");
             }
             if (rotation == Rot4.East) {
-                Log.Message("Found East Rod: " + ThingDef.Named("FishingRodEast"));
                 return ThingDef.Named("FishingRodEast");
             }
             if (rotation == Rot4.South) {
-                Log.Message("Found South Rod: " + ThingDef.Named("FishingRodSouth"));
                 return ThingDef.Named("FishingRodSouth");
             }
-            if (rotation == Rot4.West) {
-                Log.Message("Found West Rod: " + ThingDef.Named("FishingRodWest"));
-                return ThingDef.Named("FishingRodWest");
-            }
-            return ThingDef.Named("FishingRodNorth");
+            return ThingDef.Named("FishingRodWest");
         }
 
         protected override IEnumerable<Toil> MakeNewToils() {
             var i = 0;
-            Log.Message("START!");
             foreach (var toil in base.MakeNewToils()) {
-                Log.Message("Toil: " + i + " => " + toil);
                 // Hack Alert: The 13th toil is Toils_Recipe.DoRecipeWork
+                // No, there's really not an easier way to know in C#...
+                // blame anonymous functions!
                 if (i == 12) {
                     toil.AddPreInitAction(() => {
-                        Thing fishingSpot = this.TargetThingA;
-                        IntVec3 fishingSpotCell = fishingSpot.Position + new IntVec3(0, 0, -1).RotatedBy(fishingSpot.Rotation);
+                        Thing fishingSpot = this.TargetA.Thing;
+                        IntVec3 fishingSpotCell = this.TargetB.Cell;
                         ThingDef mote = GetRodMote(fishingSpot.Rotation);
                         this.fishingRod = (Mote)ThingMaker.MakeThing(mote, null);
-                        this.fishingRod.exactPosition = fishingSpotCell.ToVector3Shifted();
+                        Vector3 adjustment = new Vector3(0.0f, 0.0f, -0.25f).RotatedBy(fishingSpot.Rotation);
+                        this.fishingRod.exactPosition = fishingSpot.Position.ToVector3Shifted() + adjustment;
                         this.fishingRod.Scale = 1f;
                         GenSpawn.Spawn(this.fishingRod, fishingSpotCell, this.Map);
                     });
                     toil.AddFinishAction(() => {
-                        Thing fishingSpot = this.TargetThingA;
-                        IntVec3 fishingSpotCell = fishingSpot.Position + new IntVec3(0, 0, -1).RotatedBy(fishingSpot.Rotation);
+                        Thing fishingSpot = this.TargetA.Thing;
+                        IntVec3 fishingSpotCell = this.TargetB.Cell;
                         if (this.fishingRod != null) {
                             this.fishingRod.Destroy();
                         }
