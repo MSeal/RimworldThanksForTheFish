c7a9400fdb11e14d02b3e1854dd6857af130a31e
Matthew Seal <1863892+MSeal@users.noreply.github.com>
2018-02-16 00:37:42 -0800

    Added a minimum distance between fishing spots in the same body of water

--- ThanksForFish/ThanksForFish/AllTheFish.cs
+++ ThanksForFish/ThanksForFish/AllTheFish.cs
@@ -1,18 +1,95 @@
 ﻿using RimWorld;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using Verse;
 using Verse.AI;
 using UnityEngine;
 
 namespace AllTheFish {
+    static class Extensions {
+        public static T Pop<T>(this HashSet<T> items) {
+            T item = items.FirstOrDefault();
+            if (item != null) {
+                items.Remove(item);
+            }
+            return item;
+        }
+    }
+
     public class PlaceWorker_DeepWaterFishing : PlaceWorker {
+        public static int MIN_FISHING_RADIUS = 10;
+        public static TerrainDef Shallow = TerrainDef.Named("WaterShallow");
+        public static TerrainDef ShallowOcean = TerrainDef.Named("WaterOceanShallow");
+        public static TerrainDef ShallowMoving = TerrainDef.Named("WaterMovingShallow");
+        public static TerrainDef Deep = TerrainDef.Named("WaterDeep");
+        public static TerrainDef DeepOcean = TerrainDef.Named("WaterOceanDeep");
+        public static TerrainDef DeepMoving = TerrainDef.Named("WaterMovingDeep");
+
+        public static bool ShallowWaterTerrain(TerrainDef terrainDef) {
+            return terrainDef == Shallow || terrainDef == ShallowOcean || terrainDef == ShallowMoving;
+        }
+
+        public static bool DeepWaterTerrain(TerrainDef terrainDef) {
+            return terrainDef == Deep || terrainDef == DeepOcean || terrainDef == DeepMoving;
+        }
+
+        public static bool WaterTerrain(TerrainDef terrainDef) {
+            return ShallowWaterTerrain(terrainDef) || DeepWaterTerrain(terrainDef);
+        }
+
+        public static IEnumerable<IntVec3> DirectlyConnectedCells(IntVec3 center) {
+            yield return new IntVec3(center.x + 1, 0, center.z);
+            yield return new IntVec3(center.x, 0, center.z + 1);
+            yield return new IntVec3(center.x - 1, 0, center.z);
+            yield return new IntVec3(center.x, 0, center.z -1);
+        }
+
+        protected void AddAdjacentFringeWaterCells(IntVec3 center, HashSet<IntVec3> fringe, HashSet<IntVec3> explored, CellRect bounds, Map map) {
+            foreach (var cell in DirectlyConnectedCells(center)) {
+                if (!explored.Contains(cell) &&
+                    bounds.Contains(cell) &&
+                    cell.InBounds(map) &&
+                    WaterTerrain(map.terrainGrid.TerrainAt(cell))) {
+                    fringe.Add(cell);
+                }
+            }
+        }
+
+        protected bool CellHasFishingSpot(IntVec3 cell, Map map) {
+            foreach (var thing in map.thingGrid.ThingsListAt(cell)) {
+                if (thing.def.defName.Contains("FishingSpot")) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected bool FishingSpotConnectedByWater(IntVec3 loc, int radius, Map map) {
+            HashSet<IntVec3> explored = new HashSet<IntVec3>();
+            HashSet<IntVec3> fringe = new HashSet<IntVec3>();
+            CellRect bounds = CellRect.CenteredOn(loc, radius);
+
+            fringe.Add(loc);
+            while (fringe.Any()) {
+                IntVec3 nextCell = fringe.Pop();
+                if (CellHasFishingSpot(nextCell, map)) {
+                    return true;
+                }
+                explored.Add(nextCell);
+                AddAdjacentFringeWaterCells(nextCell, fringe, explored, bounds, map);
+            }
+
+            return false;
+        }
+
         public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null) {
-            TerrainDef terrainDef = map.terrainGrid.TerrainAt(loc);
-            if (terrainDef == TerrainDef.Named("WaterDeep") || terrainDef == TerrainDef.Named("WaterOceanDeep") || terrainDef == TerrainDef.Named("WaterMovingDeep")) {
+            if (FishingSpotConnectedByWater(loc, MIN_FISHING_RADIUS, map)) {
+                return new AcceptanceReport("AllTheFish.CloseFishing".Translate());
+            }
+            if (DeepWaterTerrain(map.terrainGrid.TerrainAt(loc))) {
                 return true;
             }
-            //TODO too close to other fishing spot
             return new AcceptanceReport("AllTheFish.DeepWaterFishing".Translate());
         }
     }
@@ -90,7 +167,7 @@ namespace AllTheFish {
             var i = 0;
             foreach (var toil in base.MakeNewToils()) {
                 // Hack Alert: The 13th toil is Toils_Recipe.DoRecipeWork
-                // No, there's really not an easier way to know in C#...
+                // No, there's really not an easier way to know in C#... that I know of
                 // blame anonymous functions!
                 if (i == 12) {
                     toil.AddPreInitAction(() => {
