09baef7b1dffecdc4287f98a5c8dba38487f1d5e
Matthew Seal <mseal007@gmail.com>
2020-08-16 15:15:51 -0700

    Fixed 1.1 assembly load

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ TARGET_DIR="/mnt/c/Program Files (x86)/Steam/steamapps/common/RimWorld/Mods/Than
 rm -rf "${TARGET_DIR}"
 mkdir "${TARGET_DIR}"
 cp -r About "${TARGET_DIR}"
-cp -r Assemblies "${TARGET_DIR}"
 cp -r 1.1 "${TARGET_DIR}"
 cp -r 1.2 "${TARGET_DIR}"
 cp -r Defs "${TARGET_DIR}"
